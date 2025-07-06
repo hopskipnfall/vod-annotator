e18b53b7b2f0e2c49d7c67c1e3b982ab76160ecb
Jonn <hopskipnfall@users.noreply.github.com>
2025-07-06 12:06:52 +0900

    Commit uncommitted stuff

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import { enableProdMode } from '@angular/core';
+import { NgModule, TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+declare const require: any;
+const translations = require(`raw-loader!./locale/messages.ja.xlf`);
+const localeId = 'ja';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {
+    providers: [
+      { provide: TRANSLATIONS, useValue: translations },
+      { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+      { provide: LOCALE_ID, useValue: localeId }
+    ]
+  })
   .catch((err) => console.error(err));
